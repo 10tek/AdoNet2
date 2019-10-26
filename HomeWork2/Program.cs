f78ad8b2444e230d2c194bec9910594a902d6f2d
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-10-27 02:32:06 +0600

    Код для создания таблицы gruppa с Id и Name

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = @"Server=DESKTOP-TALUG4B\SQLEXPRESS;Database=SalesDb;Trusted_Connection=True;";
+            var sqlQuery = "Create TABLE gruppa([Id] uniqueidentifier primary key NOT NULL, [Name] nvarchar(MAX))";
+
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = connection.CreateCommand())
+            {
+                connection.Open();
+                command.CommandText = sqlQuery;
+                using (var transaction = connection.BeginTransaction())
+                {
+                    try
+                    {
+                        command.Transaction = transaction;
+                        command.ExecuteNonQuery();
+                        transaction.Commit();
+                    }
+                    catch (SqlException exception)
+                    {
+                        Console.WriteLine(exception.Message);
+                        transaction.Rollback();
+                    }
+                }
+            }
+        }
+    }
+}
